e89889ec6d1115f0d64d92c9d9cfbbde4999bc0d
Waqas Ajmal <waqasajmal092@gmail.com>
2024-11-05 22:38:08 +0500

    first upload

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/user/{id?}', function (string $value=null) {
+    if($value){
+    return "<h1>User ID:".$value."</h1>";
+    }else{
+        return "<h1>No Id given</h1>";
+    }
+})->whereNumber('id');
+
